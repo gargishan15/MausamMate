17ce561bda004f22461a37a276ae38ecffe9eaa7
gargishan15 <gargishan2005@gmail.com>
2025-08-06 22:39:43 +0530

    Initial commit

---
+++ src/SearchBox.jsx
@@ -0,0 +1,76 @@
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import "./SearchBox.css"
+import { useState } from 'react';
+export default function SearchBox({updateInfo}){
+    let[city, setCity] = useState("");
+    let[error, setError] = useState(false);
+    const API_URL = "http://api.openweathermap.org/data/2.5/weather";
+    const API_KEY = "259c9619c0daff3280947ec0a2107b9a";
+
+    let getWeatherinfo = async() =>{
+        try{
+             let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+       let jsonResponse = await response.json();
+        
+        let result = {
+            city: city,
+            temp: jsonResponse.main.temp,
+            tempMin: jsonResponse.main.temp_min,
+            tempMax: jsonResponse.main.temp_max,
+            humidity: jsonResponse.main.humidity,
+            feelsLike: jsonResponse.main.feels_like,
+            weather: jsonResponse.weather[0].description,
+        };
+        console.log(result);
+        return result;  
+        } catch(err) {
+            throw err;
+        }
+    };
+    
+
+    let handleChange = (event) =>{
+       setCity(event.target.value);
+    };
+
+    let handleSubmit = async(event) =>{
+        try{
+        event.preventDefault();
+        console.log(city);
+        setCity("");
+        let newinfo =  await getWeatherinfo();
+        updateInfo(newinfo);
+        } catch(err){
+            setError(true);
+        }
+       
+    }
+    return(
+        <div className="SearchBox">
+            
+            <form onSubmit={handleSubmit}>
+            <TextField
+  id="city"
+  label="City Name"
+  variant="outlined"
+  required
+  value={city}
+  onChange={handleChange}
+  InputProps={{
+    style: { color: "white" }, 
+  }}
+  InputLabelProps={{
+    style: { color: "white" }, 
+  }}
+/>
+
+            <br /> <br />
+            <Button variant="contained" type="submit">
+                Search
+            </Button>
+            {error && <p style={{color: "red"}}>No such place exists!</p>}
+            </form>
+        </div>
+    )
+}
